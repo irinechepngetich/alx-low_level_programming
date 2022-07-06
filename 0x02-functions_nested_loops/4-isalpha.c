a31eef686955caf328a0f53a152ed416e4e0961d
irinechepngetich <irinechepngetich5@gmail.com>
2022-07-06 13:31:33 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+char lower, upper;
+int isletter = 0;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+if (c == lower || c == upper)
+isletter = 1;
+}
+}
+return (isletter);
+}
