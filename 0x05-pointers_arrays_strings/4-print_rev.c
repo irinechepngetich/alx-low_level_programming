d1d945662b7c9e74407d05f40774f6db55e6a4f8
irinechepngetich <irinechepngetich5@gmail.com>
2022-07-11 05:20:59 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,3 +12,17 @@ for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 _putchar('\n');
 }
+/**
+ * _strlen - returns the length of a string
+ *
+ * @str: the string whose length to return
+ *
+ * Return: the length of the string
+ */
+size_t _strlen(const char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+i++;
+return (i);
+}
