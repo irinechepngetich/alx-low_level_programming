9833b01107458b88d459148fb1ddb630c8b07cf9
irinechepngetich <irinechepngetich5@gmail.com>
2022-08-26 02:58:09 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,5 @@
-#ifndef __MAIN_H
-#define __MAIN_H
-int _putchar(char c);
+#ifndef __MAIN_H_
+#define __MAIN_H_
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
